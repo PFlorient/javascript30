891f9b331d4b964fd5fc6b243f6bcad3a745498d
Florient <flo.roku@gmail.com>
2018-11-01 22:53:59 +0100

    Animation au click

--- jour_1/script.js
+++ jour_1/script.js
@@ -1,3 +1,4 @@
+//simplification d'écriture "a la jquery"
 var $ = document.querySelector.bind(document);
 var $on = document.addEventListener.bind(document);
 
@@ -7,32 +8,47 @@ $on('mousemove', function (e) {
     posY = e.clientY || e.pageY;
 });
 
+
 var ball = $('#ball');
+//definition des variable de position+variable tampon
 var x = void 0,
     y = void 0,
     dx = void 0,
     dy = void 0,
-    tx = 0,
-    y = 0,
     key = -1;
-
+console.log(x);
 var suiveur = function suiveur() {
     key = requestAnimationFrame(suiveur);
+    
     if (!x || !y) {
+        //donne les position du curseur
         x = posX;
         y = posY;
+        
     } else {
-        dx = (posX - x) * 0.1;
-        dy = (posY - y) * 0.1;
+        //definition de la vitesse 
+        dx = (posX - x) * 0.08//+ en + rapide ;
+        dy = (posY - y) * 0.08;
         if (Math.abs(dx) + Math.abs(dy) < 0.1) {
             x = posX;
             y = posY;
         } else {
+            //met à jours la position
             x += dx;
             y += dy;
         }
     }
+
+     //Change le css de ball pour suivre le curseur
     ball.style.left = x +"px";
     ball.style.top = y + "px";
 
 };
+//aggrandit le cercle au click
+$on('click', function(e){
+    ball.style.width = "100px";
+    ball.style.height = "100px";
+    setTimeout(function(){
+        ball.style.width = "50px";
+        ball.style.height = "50px"; },1100);
+})
