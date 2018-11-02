620d737db1fe1421a795b904dfee46757306d483
Florient <flo.roku@gmail.com>
2018-11-02 23:14:50 +0100

    fin js + fin html

---
+++ day_02 calculator/script.js
@@ -0,0 +1,26 @@
+var $ = document.querySelectorAll.bind(document);
+var $on = document.addEventListener.bind(document);
+
+var x, y;
+
+function ajout(num) {
+    document.form.textview.value = document.form.textview.value + num;
+}
+
+function c() {
+    document.form.textview.value = "";
+}
+
+function bach(){
+    var text = document.form.textview.value;
+    document.form.textview.value = text.substring(0, text.length-1);
+}
+
+function equal() {
+    var isset = document.form.textview.value;
+    if (isset) {
+        document.form.textview.value = eval(document.form.textview.value);
+    }
+}
+
+//parseFloat(e.value);
