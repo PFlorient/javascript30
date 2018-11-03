8116d42f14c9d7637950c26da5d6b927025d15f5
Florient <flo.roku@gmail.com>
2018-11-03 13:55:58 +0100

    work

--- day_03 font_tester/script.js
+++ day_03 font_tester/script.js
@@ -1,2 +1,8 @@
-var $ = document.querySelectorAll.bind(document);
-var $on = document.addEventListener.bind(document);
+
+var x ;
+
+function transform() {
+     x = document.getElementById("vanillatext").value;
+     document.getElementById("changedtext").style.fontFamily = document.getElementById("fonts_list").value;
+     document.getElementById("changedtext").value = x;
+}
