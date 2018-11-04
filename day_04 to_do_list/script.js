24b2ffe26c3e6dce6e5bf9e69f79c6a596fd315e
Florient <flo.roku@gmail.com>
2018-11-04 22:57:34 +0100

    ajout validation d'une tache

--- day_04 to_do_list/script.js
+++ day_04 to_do_list/script.js
@@ -1,18 +1,27 @@
 var recuptext;
 var enter = document.getElementById("bar");
+var ul = document.getElementById("list");
 
 enter.addEventListener("keydown", function (e) {
     if (e.keyCode === 13) {
-       addmsg();
+        addmsg();
     }
 });
 
 
 function addmsg() {
     recuptext = document.getElementById("bar").value;
-    var ul = document.getElementById("list")
+
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(recuptext));
     ul.appendChild(li);
     document.getElementById("bar").value = "";
+
 }
+
+
+ul.addEventListener("click", function (e) {
+    if (e.target.tagName == "LI") {
+        e.target.classList.toggle('isok');
+    }
+})
