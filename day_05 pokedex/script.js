16e0bfb26092b6969322572382aebafeae87387e
Florient <flo.roku@gmail.com>
2018-11-05 17:59:44 +0100

    affichage de la liste + ajout du css

--- day_05 pokedex/script.js
+++ day_05 pokedex/script.js
@@ -18,10 +18,18 @@ function trouverPokemon(motentre, pokemons) {
 }
 
 
-function afficherPokemon(){
-   var trouve = trouverPokemon(this.value, name);
-   console.log(trouve);
+function afficherPokemon() {
+    var trouve = trouverPokemon(this.value, name);
+    var li = trouve.map(pokemons => {
+        return ` <li class="liste"> <div class="englobe"> <span class="numero"> ${pokemons.num} </span>  <img src="${pokemons.img}">
+        <span> ${pokemons.name}</span>
+        <span> Type : ${ pokemons.type} <span></div>
+        </li>`
+    }).join('');
+    if (search.value !="") {
+        ul.innerHTML = li;
+    }
 }
 
-search.addEventListener('change',afficherPokemon);
-search.addEventListener('keyup',afficherPokemon);
+search.addEventListener('change', afficherPokemon);
+search.addEventListener('keyup', afficherPokemon);
