42fdf213fe4884bb8112db91f54ec882082eacb9
Florient <flo.roku@gmail.com>
2018-11-05 16:47:59 +0100

    Ajout recherche javascript

--- day_05 pokedex/script.js
+++ day_05 pokedex/script.js
@@ -1,1 +1,27 @@
-var pokedex = 'https://github.com/gnufred/pgmicrotypedex/blob/master/pgdex.json';
+const pokedex = 'https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json';
+const pokemon = [];
+
+var ul = document.getElementById('list');
+var search = document.getElementById('search');
+
+fetch(pokedex)
+    .then(resp => resp.json())
+    .then(data => pokemon.push(...data.pokemon));
+console.log(pokemon);
+
+function trouverPokemon(motentre, pokemons) {
+    return pokemon.filter(pokemons => {
+
+        var word = new RegExp(motentre, 'ig');
+        return pokemons.name.match(word);
+    });
+}
+
+
+function afficherPokemon(){
+   var trouve = trouverPokemon(this.value, name);
+   console.log(trouve);
+}
+
+search.addEventListener('change',afficherPokemon);
+search.addEventListener('keyup',afficherPokemon);
