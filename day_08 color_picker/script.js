73af8622c75e024fcf8a483624a120b7e4b8bf42
Florient <flo.roku@gmail.com>
2018-11-09 17:20:11 +0100

    ok

--- day_08 color_picker/script.js
+++ day_08 color_picker/script.js
@@ -19,9 +19,9 @@ for (var i = 0; i < color.length; i++) {
     })(i);
 }
 
+//convert value background color to hexadecimal ->not me
 var hexDigits = new Array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f");
 
-//Function to convert rgb color to hex format
 function rgb2hex(rgb) {
     rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
     return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
@@ -31,6 +31,7 @@ function hex(x) {
     return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
 }
 
+//Copie value background color
 function copyToClipboard(val) {
     var couleur = document.createElement("input");
     document.body.appendChild(couleur);
@@ -40,3 +41,6 @@ function copyToClipboard(val) {
     document.body.removeChild(couleur);
 
 }
+//Fin
+//By Roku//
+//08/11/11
