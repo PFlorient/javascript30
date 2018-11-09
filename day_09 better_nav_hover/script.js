f93371c1c61994897cd1083c548f3d8786c0845e
Florient <flo.roku@gmail.com>
2018-11-09 18:49:15 +0100

    work

--- day_09 better_nav_hover/script.js
+++ day_09 better_nav_hover/script.js
@@ -9,7 +9,9 @@ function lien() {
     console.log(coordo);
     surbrillance.style.width = `${coordo.width}px`;
     surbrillance.style.height = `${coordo.height}px`;
-    
+    surbrillance.style.padding = "5px 10px ";
+    surbrillance.style.transform = `translate(${coordo.left-10}px, ${coordo.top-5}px)`;
+
 }
 
 link.forEach(a => a.addEventListener('mouseenter', lien));
