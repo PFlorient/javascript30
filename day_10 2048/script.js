65f63461fd774fe94e09c84f294c703146c2a78a
Florient <flo.roku@gmail.com>
2018-11-10 17:35:07 +0100

    work

--- day_10 2048/script.js
+++ day_10 2048/script.js
@@ -1,66 +1,224 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
-var nombrecase = 4;
-var width = canvas.width / nombrecase - 5;
+var size = 4;
+var width = canvas.width / size - 6;
+var cells = [];
+var fontSize;
+var loss = false;
+startGame();
 
-var cellule = [];
-var perdu = false;
-//créer une cellule
-function cells(row, coll) {
-    this.value = 0;
-    this.x = coll * width + 5(coll + 1);
-    this.y = row * width + 5 * (row + 1);
+
+
+function cell(row, coll) {
+  this.value = 0;
+  this.x = coll * width + 5 * (coll + 1);
+  this.y = row * width + 5 * (row + 1);
 }
-//definit permet de creer une celulle dans le carré
+
 function creercellule() {
-    for (var i = 0; i < size; i++) {
-        cellule[i] = [];
-        for (var j = 0; j < size; j++) {
-            cellule[i][j] = new cells(i, j);
-        }
+  var i, j;
+  for(i = 0; i < size; i++) {
+    cells[i] = [];
+    for(j = 0; j < size; j++) {
+      cells[i][j] = new cell(i, j);
     }
+  }
 }
-//permet d'afficher une cellule
-function voircellule(cells) {
-    ctx.beginPath();
-    ctx.rect(cells.x, cells.y, width, width);
-    switch (cells.value) {
-        case 0:
-            ctx.fillStyle = "#FFFF00";
-            break;
-        case 2:
-            ctx.fillStyle = "#FFFF00";
-            break;
-        case 4:
-            ctx.fillStyle = "#FFFF00";
-            break;
-        case 8:
-            ctx.fillStyle = "#FFFF00";
-            break;
-        case 16:
-            ctx.fillStyle = "#FFFF00";
-            break;
-        case 32:
-            ctx.fillStyle = "#FFFF00";
-            break;
-        case 64:
-            ctx.fillStyle = "#FFFF00";
-            break;
-        case 128:
-            ctx.fillStyle = "#FFFF00";
-            break;
-        case 256:
-            ctx.fillStyle = "#FFFF00";
-            break;
-        case 512:
-            ctx.fillStyle = "#FFFF00";
-            break;
-        case 1024:
-            ctx.fillStyle = "#FFFF00";
-            break;
-        case 2048:
-            ctx.fillStyle = "#FFFF00";
-            break;
+
+function drawCell(cell) {
+  ctx.beginPath();
+  ctx.rect(cell.x, cell.y, width, width);
+  switch (cell.value){
+    case 0 : ctx.fillStyle = '#ecf0f1'; break;
+    case 2 : ctx.fillStyle = '#37E87C'; break;
+    case 4 : ctx.fillStyle = '#49FFC0'; break;
+    case 8 : ctx.fillStyle = '#37E8D9'; break;
+    case 16 : ctx.fillStyle = '#3ADFFF'; break;
+    case 32 : ctx.fillStyle = '#299BE8'; break;
+    case 64 : ctx.fillStyle = '#2B75FF'; break;
+    case 128 : ctx.fillStyle = '#1B32E8'; break;
+    case 256 : ctx.fillStyle = '#3B1CFF'; break;
+    case 512 : ctx.fillStyle = '#630DE8'; break;
+    case 1024 : ctx.fillStyle = '#A80CFF'; break;
+    case 2048 : ctx.fillStyle = '#A80CFF'; break;
+    case 4096 : ctx.fillStyle = '#FF00BB'; break;
+    default : ctx.fillStyle = '#ff0080';
+  }
+  ctx.fill();
+  if (cell.value) {
+    fontSize = width / 2;
+    ctx.font = fontSize + 'px Arial';
+    ctx.fillStyle = '#414141';
+    ctx.textAlign = 'center';
+    ctx.fillText(cell.value, cell.x + width / 2, cell.y + width / 2 + width/7);
+  }
+}
+
+function canvasClean() {
+  ctx.clearRect(0, 0, 500, 500);
+}
+
+document.onkeydown = function (event) {
+  if (!loss) {
+    if (event.keyCode === 38 || event.keyCode === 87) {
+      moveUp(); 
+    } else if (event.keyCode === 39 || event.keyCode === 68) {
+      moveRight();
+    } else if (event.keyCode === 40 || event.keyCode === 83) {
+      moveDown(); 
+    } else if (event.keyCode === 37 || event.keyCode === 65) {
+      moveLeft(); 
     }
+    
+  }
+}
+
+function startGame() {
+  creercellule();
+  affichercellule();
+  ajouterunecellule();
+  ajouterunecellule();
+}
+
+function finishGame() {
+    alert('Game Over')
+  loss = true;
+}
+
+function affichercellule() {
+  var i, j;
+  for(i = 0; i < size; i++) {
+    for(j = 0; j < size; j++) {
+      drawCell(cells[i][j]);
+    }
+  }
+}
+
+function ajouterunecellule() {
+  var countFree = 0;
+  var i, j;
+  for(i = 0; i < size; i++) {
+    for(j = 0; j < size; j++) {
+      if(!cells[i][j].value) {
+        countFree++;
+      }
+    }
+  }
+  if(!countFree) {
+    finishGame();
+    return;
+  }
+  while(true) {
+    var row = Math.floor(Math.random() * size);
+    var coll = Math.floor(Math.random() * size);
+    if(!cells[row][coll].value) {
+      cells[row][coll].value = 2 * Math.ceil(Math.random() * 2);
+      affichercellule();
+      return;
+    }
+  }
+}
+
+function moveRight () {
+  var i, j;
+  var coll;
+  for(i = 0; i < size; i++) {
+    for(j = size - 2; j >= 0; j--) {
+      if(cells[i][j].value) {
+        coll = j;
+        while (coll + 1 < size) {
+          if (!cells[i][coll + 1].value) {
+            cells[i][coll + 1].value = cells[i][coll].value;
+            cells[i][coll].value = 0;
+            coll++;
+          } else if (cells[i][coll].value == cells[i][coll + 1].value) {
+            cells[i][coll + 1].value *= 2;
+            
+            cells[i][coll].value = 0;
+            break;
+          } else {
+            break;
+          }
+        }
+      }
+    }
+  }
+  ajouterunecellule();
+}
+
+function moveLeft() {
+  var i, j;
+  var coll;
+  for(i = 0; i < size; i++) {
+    for(j = 1; j < size; j++) {
+      if(cells[i][j].value) {
+        coll = j;
+        while (coll - 1 >= 0) {
+          if (!cells[i][coll - 1].value) {
+            cells[i][coll - 1].value = cells[i][coll].value;
+            cells[i][coll].value = 0;
+            coll--;
+          } else if (cells[i][coll].value == cells[i][coll - 1].value) {
+            cells[i][coll - 1].value *= 2;
+            cells[i][coll].value = 0;
+            break;
+          } else {
+            break; 
+          }
+        }
+      }
+    }
+  }
+  ajouterunecellule();
+}
+
+function moveUp() {
+  var i, j, row;
+  for(j = 0; j < size; j++) {
+    for(i = 1; i < size; i++) {
+      if(cells[i][j].value) {
+        row = i;
+        while (row > 0) {
+          if(!cells[row - 1][j].value) {
+            cells[row - 1][j].value = cells[row][j].value;
+            cells[row][j].value = 0;
+            row--;
+          } else if (cells[row][j].value == cells[row - 1][j].value) {
+            cells[row - 1][j].value *= 2;
+            cells[row][j].value = 0;
+            break;
+          } else {
+            break; 
+          }
+        }
+      }
+    }
+  }
+  ajouterunecellule();
+}
+
+function moveDown() {
+  var i, j, row;
+  for(j = 0; j < size; j++) {
+    for(i = size - 2; i >= 0; i--) {
+      if(cells[i][j].value) {
+        row = i;
+        while (row + 1 < size) {
+          if (!cells[row + 1][j].value) {
+            cells[row + 1][j].value = cells[row][j].value;
+            cells[row][j].value = 0;
+            row++;
+          } else if (cells[row][j].value == cells[row + 1][j].value) {
+            cells[row + 1][j].value *= 2;
+            cells[row][j].value = 0;
+            break;
+          } else {
+            break; 
+          }
+        }
+      }
+    }
+  }
+  ajouterunecellule();
 }
