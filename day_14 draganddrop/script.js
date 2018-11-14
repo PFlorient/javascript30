c22946e08a4b9da13879cce69009690ca2e39c59
Florient <flo.roku@gmail.com>
2018-11-14 20:57:36 +0100

    work

--- day_14 draganddrop/script.js
+++ day_14 draganddrop/script.js
@@ -4,14 +4,43 @@ const vide = document.querySelectorAll('.vide');
 fichier.addEventListener('dragstart', dragStart);
 fichier.addEventListener('dragend', dragEnd);
 
+//Définit l'action a suivre lors du drag
+for (var vidage of vide) {
+    vidage.addEventListener('dragover', dragOver);
+
+    vidage.addEventListener('dragenter', dragEnter);
+
+    vidage.addEventListener('dragleave', dragLeave);
+
+    vidage.addEventListener('drop', dragDrop);
+
+}
+
 
 function dragStart() {
     this.className += ' garder';
-    setTimeout(() =>
-        (this.className = 'invisible'), 0);
+    setTimeout(() =>(this.className = 'invisible'), 0);
 
 }
 
 function dragEnd() {
-    console.log('ok');
+    this.className = "fichier";
+}
+
+function dragOver(e){
+    e.preventDefault();
+}
+
+function dragEnter(e){
+    e.preventDefault();
+    this.className +=' hovered';
+}
+
+function dragLeave(e){
+    this.className = 'vide';
+}
+
+function dragDrop(){
+   this.className='vide';
+   this.append(fichier);
 }
