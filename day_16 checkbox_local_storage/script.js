c5cd3e9bc8a615cd71fa9ecf897787611a11e54a
Florient <flo.roku@gmail.com>
2018-11-16 23:27:23 +0100

    add text into list

--- day_16 checkbox_local_storage/script.js
+++ day_16 checkbox_local_storage/script.js
@@ -1,1 +1,28 @@
- 
+ const addItems = document.querySelector('.add-items');
+ const itemList = document.querySelector('.liste');
+ const items = [];
+
+ function addItem(e) {
+     e.preventDefault();
+     const text = (this.querySelector('[name=item]')).value;
+     const item = {
+         text,
+         done: false
+     };
+
+     items.push(item);
+     populateList(items, itemList);
+     this.reset();
+ }
+
+ function populateList(plates = [], platesList) {
+     platesList.innerHTML = plates.map((plate, i) => {
+         return `
+         <li>
+            <label for="">${plate.text}</label>
+         </li>
+         `;
+     }).join('');
+ }
+
+ addItems.addEventListener('submit', addItem)
