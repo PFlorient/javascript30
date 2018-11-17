c6ac553ac746374fd14fea6fbe0886e532545b6e
Florient <flo.roku@gmail.com>
2018-11-17 22:38:34 +0100

    work

--- day_17 api_ghibli/script.js
+++ day_17 api_ghibli/script.js
@@ -1,13 +1,39 @@
 var requete = new XMLHttpRequest();
+const app = document.getElementById('root');
+const logo = document.createElement('img');
+const container = document.createElement('div');
+container.setAttribute('class','container');
+
+logo.src = 'logo.png';
+
+app.appendChild(logo);
+app.appendChild(container);
 
 requete.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
 
 requete.onload = function () {
     var data = JSON.parse(this.response);
 
-    if (request.status >= 200 && request.status < 400) {
+    if (requete.status >= 200 && requete.status < 400) {
         data.forEach(movie => {
-          console.log(movie.title);
+            const card = document.createElement('div');
+            card.setAttribute('class','card');
+
+            const h1 = document.createElement('h1');
+            h1.textContent = movie.title;
+
+            const p = document.createElement('p');
+            movie.description = movie.description.substring(0,300);
+            p.textContent = `${movie.description}...`;
+
+            container.appendChild(card);
+
+            card.appendChild(h1);
+            card.appendChild(p);
+
+
+
+
         });
       } else {
         console.log('error');
@@ -16,4 +42,6 @@ requete.onload = function () {
 
 requete.send();
 
+
+//
 
