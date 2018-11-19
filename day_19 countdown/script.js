392f1906aa4ae24bab3f2b867fd1602d10e41c38
Florient <flo.roku@gmail.com>
2018-11-19 22:10:13 +0100

    ajout gestion minutes console

--- day_19 countdown/script.js
+++ day_19 countdown/script.js
@@ -1,14 +1,21 @@
 let countdown;
 
-function timer(seconds){
-   const now = Date.now();
-   const then = now + seconds *1000;
-   countdown = setInterval(()=>{
-        const secondLeft = Math.round((then - Date.now())/1000);
-        if(secondLeft<0){
+function timer(seconds) {
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    displayTimeLeft(seconds);
+    countdown = setInterval(() => {
+        const secondLeft = Math.round((then - Date.now()) / 1000);
+        if (secondLeft < 0) {
             clearInterval(countdown);
             return;
         }
-        console.log(secondLeft);
-   },1000);
+        displayTimeLeft(secondLeft);
+    }, 1000);
+}
+
+function displayTimeLeft(seconds){
+    const minutes = Math.floor(seconds/60);
+    const reminderSeconds = seconds%60;
+    console.log(minutes,reminderSeconds);
 }
