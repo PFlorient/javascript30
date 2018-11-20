20b9d5ae899f50d9d22f4db55ef8ada92d40eeaf
Florient <flo.roku@gmail.com>
2018-11-20 22:34:52 +0100

    preparation coord

--- day_20 stripe_menu_follow/script.js
+++ day_20 stripe_menu_follow/script.js
@@ -2,20 +2,28 @@ const triggers = document.querySelectorAll('.cool>li');
 const background = document.querySelector('.dropdownBackground');
 const nav = document.querySelector('.top');
 
-function handleEnter(){
-console.log('ENTER');
-this.classList.add('trigger-enter');
-setTimeout(()=>
-    this.classList.add('trigger-enter-active'),150);
+function handleEnter() {
+    console.log('ENTER');
+    this.classList.add('trigger-enter');
+    setTimeout(() =>
+        this.classList.add('trigger-enter-active'), 150);
+    background.classList.add('open');
 
+    const dropdown = this.querySelector('.dropdown');
+    const dropdownCoords = dropdown.getBoundingClientRect();
+    const navCoords = nav.getBoundingClientRect();
+
+    const coords = {
+        height: dropdownCoords.height,
+        width: dropdownCoords.width
+    }
 }
 
-function handleLeave(){
+function handleLeave() {
     console.log('Leave');
-
-    this.classList.remove('trigger-enter');
-
+    this.classList.remove('trigger-enter', 'trigger-enter-active');
+    background.classList.remove('open');
 }
 
-triggers.forEach(triggers=>triggers.addEventListener('mouseenter',handleEnter));
-triggers.forEach(triggers=>triggers.addEventListener('mouseleave',handleLeave));
+triggers.forEach(triggers => triggers.addEventListener('mouseenter', handleEnter));
+triggers.forEach(triggers => triggers.addEventListener('mouseleave', handleLeave));
