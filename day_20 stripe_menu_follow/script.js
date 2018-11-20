65a7e4d19f242bd3af22ae216429662cde265c5f
Florient <flo.roku@gmail.com>
2018-11-20 22:50:22 +0100

    work

--- day_20 stripe_menu_follow/script.js
+++ day_20 stripe_menu_follow/script.js
@@ -5,8 +5,10 @@ const nav = document.querySelector('.top');
 function handleEnter() {
     console.log('ENTER');
     this.classList.add('trigger-enter');
-    setTimeout(() =>
-        this.classList.add('trigger-enter-active'), 150);
+    setTimeout(() => {
+        if (this.classList.contains('trigger-enter'))
+            this.classList.add('trigger-enter-active')
+    }, 150);
     background.classList.add('open');
 
     const dropdown = this.querySelector('.dropdown');
@@ -18,12 +20,12 @@ function handleEnter() {
         width: dropdownCoords.width,
         top: dropdownCoords.top - navCoords.top,
         left: dropdownCoords.left - navCoords.left
-      };
+    };
 
-      background.style.setProperty('width', `${coords.width}px`);
-      background.style.setProperty('height', `${coords.height}px`);
-      background.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px)`);
-    }
+    background.style.setProperty('width', `${coords.width}px`);
+    background.style.setProperty('height', `${coords.height}px`);
+    background.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px)`);
+}
 
 
 function handleLeave() {
