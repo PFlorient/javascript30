25f480750b174d3484d0dab0ad7b9537ec2e9972
Florient <flo.roku@gmail.com>
2018-11-27 19:29:08 +0100

    ajout collision (non affihcer)

--- day_27 tetris/script.js
+++ day_27 tetris/script.js
@@ -4,11 +4,32 @@ const context = canvas.getContext('2d');
 context.scale(20, 20);
 
 
+
 const matrix = [
     [0, 0, 0],
     [1, 1, 1],
     [0, 1, 0],
 ];
+
+function collide(arena, player) {
+    const [m, o] = [player.matrix, player.pos];
+    for (let y = 0; y < m.length; ++y) {
+        for (let x = 0; x < m[y].length; ++x) {
+            if (m[y][x] !== 0 && (arena[y + o.y] && arena[y + o.y][x + o.x])!==0) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function createMatrix(w, h) {
+    const matrix = [];
+    while (h--) {
+        matrix.push(new Array(w).fill(0));
+    }
+    return matrix;
+}
 
 function draw() {
     context.fillStyle = '#000';
@@ -26,6 +47,27 @@ function drawMatrix(matrix, offset) {
         });
     });
 }
+
+function merge(arena, player) {
+    player.matrix.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value !== 0) {
+                arena[y + player.pos.y][x + player.pos.x] = value;
+            }
+        });
+    });
+}
+
+function playerDrop() {
+    player.pos.y++;
+    if(collide(arena,player)){
+        player.pos.y--;
+        merge(arena,player);
+        player.pos.y =0;
+    }
+    dropCounter = 0;
+}
+
 let dropCounter = 0;
 let dropInterval = 1000;
 
@@ -36,12 +78,16 @@ function update(time = 0) {
     lastTime = time;
     dropCounter += deltaTime;
     if (dropCounter > dropInterval) {
-        player.pos.y++
-        dropCounter = 0;
+        playerDrop();
     }
     draw();
     requestAnimationFrame(update);
 }
+
+const arena = createMatrix(12, 20);
+
+
+
 const player = {
     pos: {
         x: 5,
@@ -57,9 +103,8 @@ document.addEventListener('keydown', event => {
     } else
     if (event.keyCode === 39) {
         player.pos.x++;
-    }else if (event.keyCode === 40) {
-        player.pos.y++;
-        dropCounter =0;
+    } else if (event.keyCode === 40) {
+        playerDrop();
     }
 });
 
