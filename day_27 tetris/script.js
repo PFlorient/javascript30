7460e8a0fe3555c175ea6bfb7ceafd29e8c0df38
Florient <flo.roku@gmail.com>
2018-11-27 20:25:15 +0100

    ajout couleur

--- day_27 tetris/script.js
+++ day_27 tetris/script.js
@@ -36,40 +36,40 @@ function createPiece(type) {
         ];
     } else if (type === 'O') {
         return [
-            [1, 1],
-            [1, 1],
+            [2, 2],
+            [2, 2],
 
         ];
     } else if (type === 'L') {
         return [
-            [0, 1, 0],
-            [0, 1, 0],
-            [0, 1, 1],
+            [0, 3, 0],
+            [0, 3, 0],
+            [0, 3, 3],
         ];
     } else if (type === 'J') {
         return [
-            [0, 1, 0],
-            [0, 1, 0],
-            [1, 1, 0],
+            [0, 4, 0],
+            [0, 4, 0],
+            [4, 4, 0],
         ];
     } else if (type === 'I') {
         return [
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
-            [1, 1, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
         ];
     } else if (type === 'S') {
         return [
             [0, 0, 0],
-            [0, 1, 1],
-            [1, 1, 0],
+            [0, 6, 6],
+            [6, 6, 0],
         ];
     } else if (type === 'Z') {
         return [
             [0, 0, 0],
-            [1, 1, 0],
-            [0, 1, 1],
+            [7, 7, 0],
+            [0, 7, 7],
         ];
     }
 }
@@ -88,7 +88,7 @@ function drawMatrix(matrix, offset) {
     matrix.forEach((row, y) => {
         row.forEach((value, x) => {
             if (value !== 0) {
-                context.fillStyle = 'red';
+                context.fillStyle = colors[value];
                 context.fillRect(x + offset.x, y + offset.y, 1, 1);
             }
         });
@@ -173,6 +173,10 @@ function update(time = 0) {
     requestAnimationFrame(update);
 }
 
+const colors=[
+    null,'red','blue','violet','green','purple','orange','deeppink'
+]
+
 const arena = createMatrix(12, 20);
 
 function playerReset() {
@@ -180,6 +184,9 @@ function playerReset() {
     player.matrix = createPiece(pieces[pieces.length * Math.random() | 0])
     player.pos.y = 0;
     player.pos.x = (arena[0].length / 2 | 0) - (player.matrix[0].length / 2 | 0);
+    if (collide(arena, player)) {
+        arena.forEach(row => row.fill(0));
+    }
 }
 
 const player = {
