00d1d7d0c7b045798c69d01f0739067f147e9e68
Florient <flo.roku@gmail.com>
2018-11-27 19:08:57 +0100

    go down

--- day_27 tetris/script.js
+++ day_27 tetris/script.js
@@ -3,23 +3,51 @@ const context = canvas.getContext('2d');
 
 context.scale(20, 20);
 
-context.fillStyle = '#000';
-context.fillRect(0, 0, canvas.width, canvas.height);
+
 const matrix = [
     [0, 0, 0],
     [1, 1, 1],
     [0, 1, 0],
 ];
 
-function drawMatrix(matrix) {
+function draw() {
+    context.fillStyle = '#000';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    drawMatrix(player.matrix, player.pos);
+}
+
+function drawMatrix(matrix, offset) {
     matrix.forEach((row, y) => {
         row.forEach((value, x) => {
             if (value !== 0) {
                 context.fillStyle = 'red';
-                context.fillRect(x, y, 1, 1);
+                context.fillRect(x + offset.x, y + offset.y, 1, 1);
             }
         });
     });
 }
+let dropCounter = 0;
+let dropInterval = 1000;
 
-drawMatrix();
+let lastTime =0;
+function update(time = 0) {
+    const deltaTime = time - lastTime;
+    lastTime = time;
+    dropCounter += deltaTime;
+    if(dropCounter>dropInterval){
+        player.pos.y++
+        dropCounter = 0;
+    }
+    draw();
+    requestAnimationFrame(update);
+}
+const player = {
+    pos: {
+        x: 5,
+        y: 5
+    },
+    matrix: matrix,
+
+}
+
+update();
