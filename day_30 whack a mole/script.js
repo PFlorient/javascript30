051497d0d0737971b9fb2ebb3331791caae64881
Florient <flo.roku@gmail.com>
2018-11-30 22:10:44 +0100

    ajout score

--- day_30 whack a mole/script.js
+++ day_30 whack a mole/script.js
@@ -1,8 +1,10 @@
 const trous = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelectorAll('.score');
-const taupes = document.querySelectorAll('.hole');
+const taupes = document.querySelectorAll('.mole');
+let score = 0;
 
 let derniertrou;
+let timeUp = false;
 
 function randTime(min, max) {
     return Math.round(Math.random() * (max - min) + min);
@@ -11,17 +13,38 @@ function randTime(min, max) {
 function randomtrou(trous) {
     const idx = Math.floor(Math.random() * trous.length);
     const trou = trous[idx];
-
     if (trou === derniertrou) {
         console.log('cest le meme trou')
-        randomtrou(trous);
+     return  randomtrou(trous);
     }
     derniertrou = trou;
     return trou;
 }
 
-function peep(){
-    const time = randTime(200,1000);
+function peep() {
+    const time = randTime(200, 1000);
     const trou = randomtrou(trous);
-    trou.classList.add('up')
+    trou.classList.add('up');
+    setTimeout(() => {
+        trou.classList.remove('up')
+        if (!timeUp) peep();
+    }, time);
 }
+
+function startGame() {
+    scoreBoard.textContent = 0;
+    timeUp = false;
+    score = 0;
+    peep();
+    setTimeout(() => timeUp = true, 10000);
+}
+
+taupes.forEach(taupe => taupe.addEventListener('click', bonk));
+
+function bonk(e) {
+    if(!e.isTrusted) return; // cheater!
+    score++;
+    this.parentNode.classList.remove('up');
+    scoreBoard.textContent = score;
+    console.log(score);
+  }
