071454d5c3132aee213b9367f592e12153496c5a
Florient <flo.roku@gmail.com>
2018-11-30 21:12:45 +0100

    never get the same hole

--- day_30 whack a mole/script.js
+++ day_30 whack a mole/script.js
@@ -1,3 +1,21 @@
-const troue = document.querySelectorAll('.hole');
+const trous = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelectorAll('.score');
 const taupes = document.querySelectorAll('.hole');
+
+let derniertrou;
+
+function randTime(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomtrou(trous) {
+    const idx = Math.floor(Math.random() * trous.length);
+    const trou = trous[idx];
+
+    if (trou === derniertrou) {
+        console.log('cest le meme trou')
+        randomtrou(trous);
+    }
+    derniertrou = trou;
+    return trou;
+}
